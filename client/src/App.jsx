09441a1a690c90af06c1a0d48fbf221c48bad2a2
Royal Simpson Pinto <royalpinto007@gmail.com>
2023-03-21 20:56:37 +0530

    feat: Add footer section to website

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
   const [count, setCount] = useState(0)
@@ -8,7 +9,7 @@ function App() {
   return (
     <div className="App">
       <Header />
-      
+      <Footer />
     </div>
   )
 }
