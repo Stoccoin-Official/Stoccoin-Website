a9f6b93e23b1c1bb12b95bc7a3d7e9f175d7d3e1
Royal Simpson Pinto <royalpinto007@gmail.com>
2023-03-22 11:39:20 +0530

    feat: Add new section to highlight trading features and benefits.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,12 +4,15 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Stats from "./components/Stats";
 import ContactForm from './components/ContactForm';
+import Features from './components/Features';
+
 const App = () => {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
       <Header />
+      <Features />
       <Stats />
       <ContactForm />
       <Footer />
