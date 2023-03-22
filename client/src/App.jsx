ca6a49f8023ec1a1358063ce74580630d1cfbf4e
Royal Simpson Pinto <royalpinto007@gmail.com>
2023-03-22 10:54:49 +0530

    feat: Modify client package files and components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,13 +2,16 @@ import { useState } from 'react'
 import './App.css'
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-
-function App() {
+import Stats from "./components/Stats";
+import ContactForm from './components/ContactForm';
+const App = () => {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
       <Header />
+      <Stats />
+      <ContactForm />
       <Footer />
     </div>
   )
---
+++ client/src/components/ContactForm.jsx
@@ -0,0 +1,73 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div>
+      <section className="text-gray-700 body-font relative">
+        <div className="container px-5 py-24 mx-auto">
+          <div className="flex flex-col text-center w-full mb-12">
+            <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">
+              Contact Us
+            </h1>
+            <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
+              Let's trade ideas and make things happen!
+            </p>
+          </div>
+          <div className="lg:w-1/2 md:w-2/3 mx-auto">
+            <div className="flex flex-wrap -m-2">
+              <div className="p-2 w-1/2">
+                <div className="relative">
+                  <label for="name" className="leading-7 text-sm text-gray-600">
+                    Name
+                  </label>
+                  <input
+                    type="text"
+                    id="name"
+                    name="name"
+                    className="w-full bg-gray-100 rounded border border-gray-300 focus:border-indigo-500 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                  />
+                </div>
+              </div>
+              <div className="p-2 w-1/2">
+                <div className="relative">
+                  <label
+                    for="email"
+                    className="leading-7 text-sm text-gray-600"
+                  >
+                    Email
+                  </label>
+                  <input
+                    type="email"
+                    id="email"
+                    name="email"
+                    className="w-full bg-gray-100 rounded border border-gray-300 focus:border-indigo-500 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                  />
+                </div>
+              </div>
+              <div className="p-2 w-full">
+                <div className="relative">
+                  <label
+                    for="message"
+                    className="leading-7 text-sm text-gray-600"
+                  >
+                    Message
+                  </label>
+                  <textarea
+                    id="message"
+                    name="message"
+                    className="w-full bg-gray-100 rounded border border-gray-300 focus:border-indigo-500 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
+                  ></textarea>
+                </div>
+              </div>
+              <div className="p-2 w-full">
+                <button className="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+                  Submit
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+}
