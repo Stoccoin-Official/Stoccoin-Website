41250e06f616bb0379cddb60ffa17a9160e80ef5
Royal Simpson Pinto <royalpinto007@gmail.com>
2023-04-08 13:06:13 +0530

    refactor: Simplified header.jsx by removing unnecessary elements

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,8 +1,5 @@
-import { useState } from "react";
-
 export default function Example() {
-  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
-
+  
   return (
     <div className="bg-white">
       <div className="relative isolate px-6 pt-20 lg:px-8">
