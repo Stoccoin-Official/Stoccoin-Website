3b358851893feaa08b7c751f9e290c7ed8eb8d04
niranjan-kurhade <niranjandk03@gmail.com>
2023-05-04 00:11:52 +0530

    Improved the UI of the footer section

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -9,21 +9,21 @@ function Footer() {
             
           <div className="w-full md:w-1/3 px-3 mb-3 md:mb-0 text-left">
             <h2 className="text-xl font-bold text-black mb-4">About Us</h2>
-            <p className="text-gray-400 leading-loose" style={{maxWidth: "300px"}}>We're a web app for trading stocks and crypto with real-time market data and news feeds to help you make informed investment decisions.</p>
+            <p className="text-gray-600 leading-loose" style={{maxWidth: "300px"}}>We're a web app for trading stocks and crypto with real-time market data and news feeds to help you make informed investment decisions.</p>
           </div>
           
           <div className="w-full md:w-1/3 px-3 mb-3 md:mb-0 text-left">
             <h2 className="text-xl font-bold text-black mb-4">Follow Us</h2>
             <ul className="list-none">
-              <li className="mb-2"><a href="https://www.instagram.com/stoccoin/" className="text-gray-400 hover:text-white"><i className="fab fa-instagram-f"></i>Instagram</a></li>
-              <li className="mb-2"><a href="https://twitter.com/stoc_coin" className="text-gray-400 hover:text-white"><i className="fab fa-twitter"></i>Twitter</a></li>
-              <li className="mb-2"><a href="https://www.linkedin.com/company/stoccoin/" className="text-gray-400 hover:text-white"><i className="fab fa-instagram"></i>LinkedIn</a></li>
-              <li className="mb-2"><a href="https://www.kooapp.com/profile/stoccoin/" className="text-gray-400 hover:text-white"><i className="fab fa-instagram"></i>Koo</a></li>            </ul>
+              <li className="mb-2"><a href="https://www.instagram.com/stoccoin/" className="text-gray-600 hover:text-black"><i className="fab fa-instagram-f"></i>Instagram</a></li>
+              <li className="mb-2"><a href="https://twitter.com/stoc_coin" className="text-gray-600 hover:text-black"><i className="fab fa-twitter"></i>Twitter</a></li>
+              <li className="mb-2"><a href="https://www.linkedin.com/company/stoccoin/" className="text-gray-600 hover:text-black"><i className="fab fa-instagram"></i>LinkedIn</a></li>
+              <li className="mb-2"><a href="https://www.kooapp.com/profile/stoccoin/" className="text-gray-600 hover:text-black"><i className="fab fa-instagram"></i>Koo</a></li>            </ul>
           </div>
           <div className="w-full md:w-1/4 px-3 mb-3 md:mb-0 text-left">
             <h2 className="text-xl font-bold text-black mb-4">Newsletter</h2>
             <form>
-              <input type="email" placeholder="Enter your email" className="bg-gray-700 rounded py-2 px-3 text-gray-200 w-full mb-2" />
+              <input type="email" placeholder="Enter your email" className="bg-gray-200 rounded py-2 px-3 text-gray-200 w-full mb-2" />
               <button type="submit" className="bg-gray-600 hover:bg-gray-700 rounded py-2 px-4 text-white w-full">Subscribe</button>
             </form>
           </div>
