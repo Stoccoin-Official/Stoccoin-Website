e34bde5e6be8a6a647d18f8957aaddecf08d1d44
Harshita <harshitalakhchaura@gmail.com>
2023-05-20 20:20:41 +0530

    changed theme

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -3,7 +3,7 @@ import login from '../assets/login.png'
 
 const Login = () => {
   return (
-      <div className='bg-blue-200 flex h-[40rem] justify-center items-center w-[80rem] mt-10 shadow-lg'>
+      <div className='bg-indigo-400 flex h-[40rem] justify-center items-center w-[80rem] mt-10 shadow-lg'>
       <img src={login} alt="Login" className='h-[30rem]'/>
       <div className='bg-white h-[30rem] my-20 ml-32 w-[30rem] rounded-lg shadow-lg'>
         <form className='flex flex-col gap-6 m-20 text-2xl'>
@@ -25,8 +25,8 @@ const Login = () => {
             required
             className='border-b-2 border-black outline-none'
           />
-          <button type="submit" className='bg-blue-700 text-white w-[10rem] mx-20 mt-4 rounded-lg py-4 hover:bg-blue-500'>Submit</button>
-          <div className='text-xl mt-4 flex'>Don't have an account? <a href="#" className='ml-2 text-blue-600 underline cursor-pointer'>SignUp</a>
+          <button type="submit" className='bg-[#4f46e5] text-white w-[10rem] mx-20 mt-4 rounded-lg py-4 hover:bg-[#818cf8]'>Submit</button>
+          <div className='text-xl mt-4 flex'>Don't have an account? <a href="#" className='ml-2 text-blue-600 underline cursor-pointer'>Sign-Up</a>
           </div>
         </form>
       </div>
