c9f98d8c2af05e38c7a1d711c481a00e1f6cc0a5
Royal Simpson Pinto <royalpinto007@gmail.com>
2023-05-21 12:14:44 +0530

    Revert "Linked Routes"

--- client/src/components/Features.jsx
+++ client/src/components/Features.jsx
@@ -1,52 +1,43 @@
-import { FingerPrintIcon, LockClosedIcon } from "@heroicons/react/24/outline";
-import {
-  BookOpenIcon,
-  ChartBarIcon,
-  ArrowsUpDownIcon,
-} from "@heroicons/react/24/solid";
+import { FingerPrintIcon, LockClosedIcon } from '@heroicons/react/24/outline'
+import { BookOpenIcon, ChartBarIcon, ArrowsUpDownIcon} from '@heroicons/react/24/solid'
 const features = [
   {
-    name: "Open Source",
+    name: 'Open Source',
     description:
-      "More eyes on the code, more contributors, and faster resolution of security vulnerabilities.",
+      'More eyes on the code, more contributors, and faster resolution of security vulnerabilities.',
     icon: BookOpenIcon,
   },
   {
-    name: "Real-time market data",
+    name: 'Real-time market data',
     description:
-      "Immediate and continuous updating of financial information as it occurs in the market.",
+      'Immediate and continuous updating of financial information as it occurs in the market.',
     icon: ArrowsUpDownIcon,
   },
   {
-    name: "Advanced charting and analytics",
+    name: 'Advanced charting and analytics',
     description:
-      "Access market analysis using charts & analytics tools for informed decision-making.",
+      'Access market analysis using charts & analytics tools for informed decision-making.',
     icon: ChartBarIcon,
   },
   {
-    name: "SSL certificates",
+    name: 'SSL certificates',
     description:
-      "Secure Socket Layer (SSL) encryption to protect user data and transactions.",
+      'Secure Socket Layer (SSL) encryption to protect user data and transactions.',
     icon: LockClosedIcon,
   },
-];
+]
 
 export default function Example() {
   return (
-    <div className="bg-white sm:py-14" id="features">
+    <div className="bg-white sm:py-14">
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
         <div className="mx-auto max-w-2xl lg:text-center">
-          <h2 className="text-base font-semibold leading-7 text-indigo-600">
-            Trade Boosters
-          </h2>
-          <h2 className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-            Streamlined Trading Features
-          </h2>
+          <h2 className="text-base font-semibold leading-7 text-indigo-600">Trade Boosters</h2>
+            <h2 className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+              Streamlined Trading Features
+            </h2> 
           <p className="mt-6 text-lg leading-8 text-gray-600">
-            Explore the comprehensive list of features that make our trading
-            platform stand out from the rest. Our powerful tools and advanced
-            technologies are designed to enhance your trading experience and
-            give you an edge in the market.
+            Explore the comprehensive list of features that make our trading platform stand out from the rest. Our powerful tools and advanced technologies are designed to enhance your trading experience and give you an edge in the market.
           </p>
         </div>
         <div className="mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-4xl">
@@ -55,21 +46,16 @@ export default function Example() {
               <div key={feature.name} className="relative pl-16">
                 <dt className="text-base font-semibold leading-7 text-gray-900">
                   <div className="absolute top-0 left-0 flex h-10 w-10 items-center justify-center rounded-lg bg-indigo-600">
-                    <feature.icon
-                      className="h-6 w-6 text-white"
-                      aria-hidden="true"
-                    />
+                    <feature.icon className="h-6 w-6 text-white" aria-hidden="true" />
                   </div>
                   {feature.name}
                 </dt>
-                <dd className="mt-2 text-base leading-7 text-gray-600">
-                  {feature.description}
-                </dd>
+                <dd className="mt-2 text-base leading-7 text-gray-600">{feature.description}</dd>
               </div>
             ))}
           </dl>
         </div>
       </div>
     </div>
-  );
+  )
 }
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,45 +1,42 @@
 export default function Example() {
+  
   return (
-    <div className="bg-white" id="header">
+    <div className="bg-white">
       <div className="relative isolate px-6 pt-20 lg:px-8">
-        <div className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"></div>
-        <div className="container mx-auto flex">
-          <div className="max-w-xl pt-20 pl-10 pr-60">
-            <div className="text-left">
-              <h1
-                className="text-4xl font-extrabold tracking-tight text-gray-900 sm:text-6xl"
-                style={{ fontFamily: "'Open Sans Extra Bold', sans-serif" }}
-              >
-                Stoccoin
-              </h1>
-              <p className="mt-6 text-lg leading-8 text-gray-600">
-                Learn-Invest-Repeat
-              </p>
-              <div className="mt-10 flex items-center justify-left gap-x-6">
-                <a
-                  href="#"
-                  className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                >
-                  Get started
-                </a>
-                <a
-                  href="#"
-                  className="text-sm font-semibold leading-6 text-gray-900"
-                >
-                  Learn more <span aria-hidden="true">→</span>
-                </a>
+        <div className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80">
+          
+          </div>
+            <div className="container mx-auto flex">
+              <div className="max-w-xl pt-20 pl-10 pr-60">
+                <div className="text-left">
+                  <h1
+                    className="text-4xl font-extrabold tracking-tight text-gray-900 sm:text-6xl"
+                    style={{ fontFamily: "'Open Sans Extra Bold', sans-serif" }}
+                  >
+                    Stoccoin
+                  </h1>
+                  <p className="mt-6 text-lg leading-8 text-gray-600">
+                    Learn-Invest-Repeat
+                  </p>
+                  <div className="mt-10 flex items-center justify-left gap-x-6">
+                    <a
+                      href="#"
+                      className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                    >
+                      Get started
+                    </a>
+                    <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
+                      Learn more <span aria-hidden="true">→</span>
+                    </a>
+                  </div>
+                </div>
+              </div>
+              <div pt-50 style={{ float: 'right' }}>
+                <img src="./header.svg" alt="your-image-description" style={{ maxHeight: '50rem', maxWidth: '35rem' }}/>
               </div>
             </div>
-          </div>
-          <div pt-50 style={{ float: "right" }}>
-            <img
-              src="./header.svg"
-              alt="your-image-description"
-              style={{ maxHeight: "50rem", maxWidth: "35rem" }}
-            />
-          </div>
+          <div className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]">  
         </div>
-        <div className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"></div>
       </div>
     </div>
   );
