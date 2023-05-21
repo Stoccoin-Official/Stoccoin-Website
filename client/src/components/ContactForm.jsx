c062cc554ed28c1394c431116c8fb235a362f6ee
pooranjoyb <pooranjoyb2016@gmail.com>
2023-05-21 19:27:33 +0530

    bug fixes

--- client/src/components/ContactForm.jsx
+++ client/src/components/ContactForm.jsx
@@ -42,37 +42,46 @@ export default function App() {
           <img src="./contactus.svg" alt="" />
         </div>
       </div>
-      <div className="">
-        <div>
-          <span className="text-sm font-bold uppercase text-gray-600">
-            Full Name
-          </span>
-          <input
-            className="focus:shadow-outline mt-2 w-full rounded-lg bg-gray-300 p-3 text-gray-900 focus:outline-none"
-            type="text"
-            placeholder=""
-          />
+      <form onSubmit={handleSubmit}>
+        <div className="">
+          <div>
+            <span className="text-sm font-bold uppercase text-gray-600">
+              Full Name
+            </span>
+            <input
+              className="focus:shadow-outline mt-2 w-full rounded-lg bg-gray-300 p-3 text-gray-900 focus:outline-none"
+              type="text"
+              placeholder=""
+              onChange={(e) => setName(e.target.value)}
+            />
+          </div>
+          <div className="mt-8">
+            <span className="text-sm font-bold uppercase text-gray-600">
+              Email
+            </span>
+            <input
+              className="focus:shadow-outline mt-2 w-full rounded-lg bg-gray-300 p-3 text-gray-900 focus:outline-none"
+              type="email"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div className="mt-8">
+            <span className="text-sm font-bold uppercase text-gray-600">
+              Message
+            </span>
+            <textarea
+              className="focus:shadow-outline mt-2 h-32 w-full rounded-lg bg-gray-300 p-3 text-gray-900 focus:outline-none"
+              defaultValue={""}
+              onChange={(e) => setMessage(e.target.value)}
+            />
+          </div>
+          <div className="mt-8">
+            <button className="focus:shadow-outline w-full rounded-lg bg-indigo-500 p-3 text-sm font-bold uppercase tracking-wide text-gray-100 focus:outline-none">
+              Send Message
+            </button>
+          </div>
         </div>
-        <div className="mt-8">
-          <span className="text-sm font-bold uppercase text-gray-600">Email</span>
-          <input
-            className="focus:shadow-outline mt-2 w-full rounded-lg bg-gray-300 p-3 text-gray-900 focus:outline-none"
-            type="text"
-          />
-        </div>
-        <div className="mt-8">
-          <span className="text-sm font-bold uppercase text-gray-600">Message</span>
-          <textarea
-            className="focus:shadow-outline mt-2 h-32 w-full rounded-lg bg-gray-300 p-3 text-gray-900 focus:outline-none"
-            defaultValue={""}
-          />
-        </div>
-        <div className="mt-8">
-          <button className="focus:shadow-outline w-full rounded-lg bg-indigo-500 p-3 text-sm font-bold uppercase tracking-wide text-gray-100 focus:outline-none">
-            Send Message
-          </button>
-        </div>
-      </div>
+      </form>
     </div>
   );
 }
