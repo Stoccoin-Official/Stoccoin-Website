22ed8223c38cd5bf4f407a3507ea69571623d81e
The-Chimsom <okorogladys52@gmail.com>
2023-05-22 12:26:45 -0700

    move thee user file into the authentication folder

--- server/App.js
+++ server/App.js
@@ -6,7 +6,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var usersRouter = require('./authentication/users');
 var loginRouter = require('./authentication/authRouter')
 var mailRouter = require('./routes/mail')
 
