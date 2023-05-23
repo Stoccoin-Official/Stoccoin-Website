111f81d185934c6ac08d342e0e3be1855fe97cea
Mahendra Dani <mahendraunofficial09@gmail.com>
2023-05-23 23:12:21 +0530

    docs: Add contributing rule in README.md

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,15 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Stats from "./components/Stats";
-import ContactForm from './components/ContactForm';
-import Features from './components/Features';
-import Accordion from './components/Accordion';
-import Pricing from './components/Pricing';
-import Navbar from './components/Navbar';
+import ContactForm from "./components/ContactForm";
+import Features from "./components/Features";
+import Accordion from "./components/Accordion";
+import Pricing from "./components/Pricing";
+import Navbar from "./components/Navbar";
 const App = () => {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div className="App">
@@ -20,9 +20,9 @@ const App = () => {
       <Pricing />
       <Accordion />
       <ContactForm />
-      <Footer />   
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
