aee3a0f768e73f4a4fd8a53dab4e721757c75c1a
Mahendra Dani <mahendraunofficial09@gmail.com>
2023-05-28 09:11:02 +0530

    bug: Resolved confilcts with this branch

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -84,7 +84,9 @@ export default function Example() {
           onClose={setMobileMenuOpen}
         >
           <div className="fixed inset-0 z-10" />
-          <Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+          <Dialog.Panel
+            className={`fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10`}
+          >
             <div className="flex items-center justify-between">
               <a href="#" className="-m-1.5 p-1.5">
                 <span className="sr-only">Stoccoin</span>
