12555e97659e7ac8040b65555e53c65ffaa0cbd2
Royal Simpson Pinto <royalpinto007@gmail.com>
2023-06-18 15:37:10 +0530

    feat: Added widgets to cryptocurrencies page

---
+++ client/src/components/TradingViewWidget.jsx
@@ -0,0 +1,58 @@
+import React, { useEffect, useRef } from 'react';
+
+let tvScriptLoadingPromise;
+
+export default function TradingViewWidget() {
+  const onLoadScriptRef = useRef();
+
+  useEffect(
+    () => {
+      onLoadScriptRef.current = createWidget;
+
+      if (!tvScriptLoadingPromise) {
+        tvScriptLoadingPromise = new Promise((resolve) => {
+          const script = document.createElement('script');
+          script.id = 'tradingview-widget-loading-script';
+          script.src = 'https://s3.tradingview.com/tv.js';
+          script.type = 'text/javascript';
+          script.onload = resolve;
+
+          document.head.appendChild(script);
+        });
+      }
+
+      tvScriptLoadingPromise.then(() => onLoadScriptRef.current && onLoadScriptRef.current());
+
+      return () => onLoadScriptRef.current = null;
+
+      function createWidget() {
+        if (document.getElementById('tradingview_7d84a') && 'TradingView' in window) {
+          new window.TradingView.widget({
+            width: 1000,
+            height: 600,
+            symbol: "NASDAQ:AAPL",
+            interval: "D",
+            timezone: "Etc/UTC",
+            theme: "light",
+            style: "1",
+            locale: "in",
+            toolbar_bg: "#f1f3f6",
+            enable_publishing: false,
+            allow_symbol_change: true,
+            container_id: "tradingview_7d84a"
+          });
+        }
+      }
+    },
+    []
+  );
+
+  return (
+    <div className='tradingview-widget-container'>
+      <div id='tradingview_7d84a' />
+      <div className="tradingview-widget-copyright">
+        <a href="https://in.tradingview.com/" rel="noopener nofollow" target="_blank"><span className="blue-text">Track all markets on TradingView</span></a>
+            </div>
+    </div>
+  );
+}
--- client/src/pages/Cryptocurrencies.jsx
+++ client/src/pages/Cryptocurrencies.jsx
@@ -1,17 +1,17 @@
 import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+import TradingViewWidget from "../components/TradingViewWidget";
+import TradingViewWidget1 from "../components/TradingViewWidget1";
+
 const Cryptocurrencies = () => {
   return (
     <div>
       <Navbar />
-      <h1 className="text-5xl text-center p-8 m-8">Cryptocurrencies</h1>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate,
-        placeat blanditiis, est non atque cum aut ratione pariatur quas eligendi
-        iure earum illo quos aspernatur beatae nesciunt expedita. Quidem nam ad
-        illo blanditiis optio consequuntur quis veritatis laudantium cum
-        consectetur animi magni nemo eveniet, reiciendis expedita, aperiam
-        laborum ut quaerat?
-      </p>
+        <h6 className="text-3xl text-left p-8 m-8">Cryptocurrencies Market Overview</h6>
+      <TradingViewWidget1 />
+      <br/>
+      <TradingViewWidget />
+      <Footer />
     </div>
   );
 };
