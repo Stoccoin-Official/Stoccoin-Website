70a592e33fa70ebf773730d4f8cef5dca3d5b167
Royal Simpson Pinto <royalpinto007@gmail.com>
2023-07-08 18:17:12 +0530

    feat: modified stocks widget

--- client/src/components/stocks/Widget1.jsx
+++ client/src/components/stocks/Widget1.jsx
@@ -5,24 +5,114 @@ const Widget1 = () => {
     const script = document.createElement('script');
     script.src = 'https://s3.tradingview.com/external-embedding/embed-widget-market-quotes.js';
     script.async = true;
-    script.innerHTML = JSON.stringify({
-      width: 1000,
-      height: 800,
-      defaultColumn: 'overview',
-      screener_type: 'most_actives',
-      displayCurrency: 'USD',
-      colorTheme: 'light',
-      locale: 'in',
-    });
+    script.innerHTML = `{
+      "title": "Stocks",
+      "width": 1000,
+      "height": 800,
+      "locale": "in",
+      "showSymbolLogo": true,
+      "symbolsGroups": [
+        {
+          "name": "Financial",
+          "symbols": [
+            {
+              "name": "NYSE:JPM",
+              "displayName": "Jpmorgan Chase & Co"
+            },
+            {
+              "name": "NYSE:WFC",
+              "displayName": "Wells Fargo Co New"
+            },
+            {
+              "name": "NYSE:BAC",
+              "displayName": "Bank Amer Corp"
+            },
+            {
+              "name": "NYSE:HSBC",
+              "displayName": "Hsbc Hldgs Plc"
+            },
+            {
+              "name": "NYSE:C",
+              "displayName": "Citigroup Inc"
+            },
+            {
+              "name": "NYSE:MA",
+              "displayName": "Mastercard Incorporated"
+            }
+          ]
+        },
+        {
+          "name": "Technology",
+          "symbols": [
+            {
+              "name": "NASDAQ:AAPL",
+              "displayName": "Apple"
+            },
+            {
+              "name": "NASDAQ:GOOGL",
+              "displayName": "Google Inc"
+            },
+            {
+              "name": "NASDAQ:MSFT",
+              "displayName": "Microsoft Corp"
+            },
+            {
+              "name": "NASDAQ:FB",
+              "displayName": "Meta Platforms, Inc"
+            },
+            {
+              "name": "NYSE:ORCL",
+              "displayName": "Oracle Corp"
+            },
+            {
+              "name": "NASDAQ:INTC",
+              "displayName": "Intel Corp"
+            }
+          ]
+        },
+        {
+          "name": "Services",
+          "symbols": [
+            {
+              "name": "NASDAQ:AMZN",
+              "displayName": "Amazon Com Inc"
+            },
+            {
+              "name": "NYSE:BABA",
+              "displayName": "Alibaba Group Hldg Ltd"
+            },
+            {
+              "name": "NYSE:T",
+              "displayName": "At&t Inc"
+            },
+            {
+              "name": "NYSE:WMT",
+              "displayName": "Wal-mart Stores Inc"
+            },
+            {
+              "name": "NYSE:V",
+              "displayName": "Visa Inc"
+            }
+          ]
+        }
+      ],
+      "colorTheme": "light"
+    }`;
 
-    const widgetContainer = document.getElementById('tradingview-widget');
-    if (widgetContainer && widgetContainer.childElementCount === 0) {
-      widgetContainer.appendChild(script);
+    const widgetContainer = document.createElement('div');
+    widgetContainer.className = 'tradingview-widget-container';
+   
+
+    widgetContainer.appendChild(script);
+
+    const container = document.getElementById('tradingview-widget');
+    if (container && container.childElementCount === 0) {
+      container.appendChild(widgetContainer);
     }
 
     return () => {
-      if (widgetContainer) {
-        widgetContainer.innerHTML = '';
+      if (container) {
+        container.innerHTML = '';
       }
     };
   }, []);
