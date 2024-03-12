8b9ab5df960b0cc8fbd2dc0f52de4d357f81b5a0
Mohit <mrai645@gmail.com>
2024-03-12 21:47:12 +0530

    changes made instead of lorem

--- app/src/pages/Dashboard.jsx
+++ app/src/pages/Dashboard.jsx
@@ -3,12 +3,7 @@ const Dashboard = () => {
     <div className="dark:bg-slate-900 flex justify-center flex-col items-center">
       <h1 className="text-5xl p-14 m-8 dark:text-cyan-100 ">Dashboard</h1>
       <p className="dark:text-cyan-100 text-center w-[1100px]">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate,
-        placeat blanditiis, est non atque cum aut ratione pariatur quas eligendi
-        iure earum illo quos aspernatur beatae nesciunt expedita. Quidem nam ad
-        illo blanditiis optio consequuntur quis veritatis laudantium cum
-        consectetur animi magni nemo eveniet, reiciendis expedita, aperiam
-        laborum ut quaerat?
+        The work is in progress
       </p>
     </div>
   );
